163fbf02d529bcf7757b29e3fe233fb6a4087267
Sophia Yougin <sophiya273@gmail.com>
2021-01-14 20:31:40 +0530

    css added for second page

--- src/ShowDetails.js
+++ src/ShowDetails.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import Images from './Images';
 class ShowDetails extends React.Component{
   constructor(){
     super();
@@ -11,15 +10,20 @@ class ShowDetails extends React.Component{
   }
   render(){
     const data = {...this.state.show};
-    console.log(this.state.isLoading)
+    console.log(data)
     return(
         this.state.isLoading?<h1 style={{color:'white',textAlign:'center'}}>Loading...</h1>:
         <div className="show-details-page">
-            <h1>{data.name}</h1> 
-            <h4>language: {data.language}</h4>
-            <h4>year: {data.premiered}</h4>
-            <p>{data.summary}</p>
-            <img src={data.image.medium}/>
+            <img src={data.image.medium} alt=''/>
+            <div className='details'>
+              <h1>{data.name}</h1> 
+              <h4>Genre: {(data.genres.map(genre=>genre)).join(', ')}</h4>
+              <h4>Language: {data.language}</h4>
+              <h4>Year: {data.premiered.slice(0,4)}</h4>
+              <div className='site-link'><h4>Official Site:</h4><a target="_blank" href={data.officialSite}>{data.officialSite}</a></div>
+              <h4>Rating: {data.rating.average}</h4>
+              <div dangerouslySetInnerHTML={{__html: data.summary}}></div>
+            </div>
         </div>
     );
   }
